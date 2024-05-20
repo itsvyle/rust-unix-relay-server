918822b406d8944caaa52828af8aa2e183b31e34
Greg Mat. <65409906+itsvyle@users.noreply.github.com>
2024-05-20 14:45:29 +0000

    Added max message size of 1024 bytes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # rust-unix-relay-server
+
 A simple server to relay messages sent to a unix socket
+
+Test it by running:
+
+```bash
+socat -d -d - UNIX-CONNECT:/tmp/relay_socket
+```
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ use tokio::sync::{broadcast, Mutex};
 use std::sync::Arc;
 use uuid::Uuid;
 
+const MAX_MESSAGE_SIZE: usize = 1024; // in bytes
+
 #[tokio::main]
 async fn main() -> tokio::io::Result<()> {
-    let path = "/var/relay_socket";
-    let _ = std::fs::remove_file(path);
+    let path = "/tmp/relay_socket";
+    let _ = std::fs::remove_file(path); // remove old one
     let listener = UnixListener::bind(path)?;
+    eprintln!("Listening on: {:?} at socket path {}", listener.local_addr()?, path);
 
     // Create a broadcast channel with a buffer size of 16.
     let (tx, _rx) = broadcast::channel(16);
@@ -40,9 +43,9 @@ async fn handle_connection(
 ) {
     let client_id = Uuid::new_v4();
     clients.lock().await.push(client_id);
-    eprintln!("Client {} connected", client_id);
+    eprintln!("Client {} connected, now have {} clients connected", client_id, clients.lock().await.len());
 
-    let mut buffer = vec![0; 1024];
+    let mut buffer = vec![0; MAX_MESSAGE_SIZE];
 
 
     loop {
@@ -50,14 +53,19 @@ async fn handle_connection(
             result = socket.read(&mut buffer) => {
                 match result {
                     Ok(0) => {
+                        eprintln!("Client {} closed conn", client_id);
                         break; // connection has been closed
                     }
                     Ok(n) => {
+                        if n >= MAX_MESSAGE_SIZE {
+                            eprintln!("Received message exceeds maximum allowed size ({} bytes, client {})", n, client_id);
+                            break;
+                        }
+
                         let msg = buffer[..n].to_vec();
                         if tx.send((client_id, msg)).is_err() {
                             eprintln!("There are no active receivers; dropping message")
                         }
-                        buffer.clear();
                     }
                     Err(e) => {
                         eprintln!("Failed to read from socket; err = {:?}", e);
@@ -84,7 +92,6 @@ async fn handle_connection(
         }
     }
 
-    // Remove the client from the list
     clients.lock().await.retain(|&id| id != client_id);
-    eprintln!("Client {} disconnected", client_id);
+    eprintln!("Client {} disconnected, now have {} clients connected", client_id, clients.lock().await.len());
 }
